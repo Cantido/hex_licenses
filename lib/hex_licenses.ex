67faa4df6189fafb01837cd028403c45a6b89ac6
Rosa Richter <rosa@prokeep.com>
2021-09-09 15:05:22 -0600

    New Mix project

---
+++ lib/hex_licenses.ex
@@ -0,0 +1,18 @@
+defmodule HexLicenses do
+  @moduledoc """
+  Documentation for `HexLicenses`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> HexLicenses.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/hex_licenses_test.exs
@@ -0,0 +1,8 @@
+defmodule HexLicensesTest do
+  use ExUnit.Case
+  doctest HexLicenses
+
+  test "greets the world" do
+    assert HexLicenses.hello() == :world
+  end
+end
