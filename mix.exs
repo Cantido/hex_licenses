04bc5622cca0849d05b6f20309a605f8cd8a404b
Rosa Richter <cosmic.lady.rosa@gmail.com>
2021-09-10 10:05:53 -0600

    Fix mix.exs package info

--- mix.exs
+++ mix.exs
@@ -11,6 +11,7 @@ defmodule HexLicenses.MixProject do
       version: "0.1.0",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
+      package: package(),
       deps: deps()
     ]
   end
