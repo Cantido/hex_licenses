92f02870ac5358b4520554b8fbffc3a9bf788672
Rosa Richter <cosmic.lady.rosa@gmail.com>
2021-09-11 13:03:33 -0600

    Update explain moduledoc to include OSI option

--- lib/mix/tasks/licenses/explain.ex
+++ lib/mix/tasks/licenses/explain.ex
@@ -9,24 +9,38 @@ defmodule Mix.Tasks.Licenses.Explain do
   It is not mandatory for a package's `:licenses` value to contain only SPDX license identifiers, it is only recommended.
   Therefore, some problems reported by `mix licenses` may be false alarms.
   For example, a project may specify its license as `MIT License` rather than the SPDX identifier `MIT`.
+
+  ## Command line options
+
+    * `--osi` - additionally check if all licenses are approved by the [Open Source Initiative](https://opensource.org/licenses)
   """
 
   use Mix.Task
 
   @shortdoc "Prints all dependencies with unrecognized or non-OSI-approved licenses."
 
-  def run(_args) do
+  def run(args) do
+    check_osi_approved = "--osi" in args
+
+    allowed_statuses =
+          if check_osi_approved do
+            [:osi_approved]
+          else
+            [:osi_approved, :not_approved]
+          end
     unsafe_deps =
       HexLicenses.license_check()
       |> Enum.reject(fn {_deps, licenses} -> licenses == :not_in_hex end)
       |> Enum.filter(fn {_dep, licenses} ->
-        Enum.any?(licenses, fn {_license, status} -> status != :osi_approved end)
+        Enum.any?(licenses, fn {_license, status} ->
+          status not in allowed_statuses
+        end)
       end)
 
     Enum.sort_by(unsafe_deps, fn {dep, _licenses} -> to_string(dep) end)
     |> Enum.each(fn {dep, licenses} ->
       unsafe_licenses =
-        Enum.filter(licenses, fn {_license, status} -> status != :osi_approved end)
+        Enum.filter(licenses, fn {_license, status} -> status not in allowed_statuses end)
 
       Mix.shell().info("#{dep} has #{Enum.count(unsafe_licenses)} unsafe licenses:")
 
@@ -36,7 +50,11 @@ defmodule Mix.Tasks.Licenses.Explain do
     end)
 
     if Enum.empty?(unsafe_deps) do
-      Mix.shell().info("All dependencies have OSI-approved licenses.")
+      if check_osi_approved do
+        Mix.shell().info("All dependencies have OSI-approved licenses.")
+      else
+        Mix.shell().info("All dependencies have recognized licenses.")
+      end
     else
       exit({:shutdown, 1})
     end
